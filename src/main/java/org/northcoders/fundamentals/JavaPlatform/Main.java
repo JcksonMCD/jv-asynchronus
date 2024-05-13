adb59ef07e3b6bcd4a6830adbf17bf543ff7d210
Jackson MacDonald <jacksonmacdonald@gmail.com>
2024-05-13 11:36:28 +0100

    Feat: EXC1 complete

--- src/main/java/org/northcoders/fundamentals/JavaPlatform/Main.java
+++ src/main/java/org/northcoders/fundamentals/JavaPlatform/Main.java
@@ -1,7 +1,21 @@
 package org.northcoders.fundamentals.JavaPlatform;
 
+import java.util.concurrent.CompletableFuture;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+
+        // Print "Hello World!" to the console asynchronously after a delay of two seconds using a CompletableFuture.
+        CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Hello World!");
+        });
+
+        Thread.sleep(5000);
+
     }
 }
